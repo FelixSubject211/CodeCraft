55f4015383c0658d99c3a8bc31cd923eaa20502c
FelixSubject211 <113304291+FelixSubject211@users.noreply.github.com>
2024-09-21 16:55:15 +0200

    Initial Commit

---
+++ CodeCraft/CodeCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCraftApp.swift
+//  CodeCraft
+//
+//  Created by Felix Fischer on 21.09.24.
+//
+
+import SwiftUI
+
+@main
+struct CodeCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
